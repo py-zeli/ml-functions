45d5a2f2df0a6e4be02f713063bb6d57bce2122d
Kauan Pizeli <kauan.pizeli@gmail.com>
2026-05-09 22:27:15 -0300

    Gráfico criado para complementar teste de função Gradient Descent

--- teste.py
+++ teste.py
@@ -3,26 +3,27 @@ import functions
 x = [2, 3, 5, 7, 8]
 y = [5, 7, 8, 10, 12]
 
-# print(functions.media(x))
+coef_angular, coef_linear = functions.minimos_quadrados(x,y)
 
-# coef_angular, coef_linear = functions.minimos_quadrados(x,y)
+y_pred = functions.realizar_previsao(x, coef_angular, coef_linear)
 
-# y_pred = functions.realizar_previsao(x, coef_angular, coef_linear)
+print(f"Coeficiente Linear: {coef_linear}\nCoeficiente Angular: {coef_angular}\n")
 
-# print(functions.rmse(y, y_pred))
+
+print(functions.rmse(y, y_pred))
 
 # print(functions.gradient_descent(x, y, 0.01))
 
-for i in range(5):
+# for i in range(5):
 
-    if 'novo_coef_linear' in locals():
-        valores_calibrados = functions.gradient_descent(x, y, 0.01, novo_coef_linear, novo_coef_angular)
+#     if 'novo_coef_linear' in locals():
+#         valores_calibrados = functions.gradient_descent(x, y, 0.01, novo_coef_linear, novo_coef_angular)
     
-    else:
-        valores_calibrados = functions.gradient_descent(x, y, 0.01)
+#     else:
+#         valores_calibrados = functions.gradient_descent(x, y, 0.01)
     
 
-    print(f"Resultados da {i+1} iteração:\n{valores_calibrados}\n")
-    # # valores_calibrados = print(functions.gradient_descent(x, y, 0.01))
-    novo_coef_linear = valores_calibrados['novo_coef_linear']
-    novo_coef_angular = valores_calibrados['novo_coef_angular']
+#     print(f"Resultados da {i+1} iteração:\n{valores_calibrados}\n")
+#     # # valores_calibrados = print(functions.gradient_descent(x, y, 0.01))
+#     novo_coef_linear = valores_calibrados['novo_coef_linear']
+#     novo_coef_angular = valores_calibrados['novo_coef_angular']
